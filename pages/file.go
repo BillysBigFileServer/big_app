b9ed9691eae0edf4a9eb1501c2d6e0c50a1f1937
Billy Batista <bootlegbilly@protonmail.ch>
2024-10-02 14:06:32 -0400

    add a "share" button
    for now, this just copies the share link to the system clipboard

--- pages/file.go
+++ pages/file.go
@@ -2,13 +2,18 @@ package pages
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 	"github.com/BillysBigFileServer/app/helper"
+	"github.com/BillysBigFileServer/app/preferences"
 	"github.com/BillysBigFileServer/bfsp-go"
+	"github.com/BillysBigFileServer/bfsp-go/config"
 )
 
 func FilePage(ctx context.Context, fileMeta *bfsp.FileMetadata, w fyne.Window) fyne.CanvasObject {
@@ -21,7 +26,14 @@ func FilePage(ctx context.Context, fileMeta *bfsp.FileMetadata, w fyne.Window) f
 	if err != nil {
 		panic(err)
 	}
+	shareIcon, err := fyne.LoadResourceFromPath("resources/share.png")
+	if err != nil {
+		panic(err)
+	}
 
+	backButton := widget.NewButtonWithIcon("", backArrow, func() {
+		w.SetContent(FilesPage(ctx, w, false))
+	})
 	downloadButton := widget.NewButtonWithIcon("", downloadIcon, func() {
 		fileSaver := dialog.NewFileSave(func(uri fyne.URIWriteCloser, err error) {
 			err = bfsp.DownloadFile(ctx, fileMeta, uri, "")
@@ -32,11 +44,39 @@ func FilePage(ctx context.Context, fileMeta *bfsp.FileMetadata, w fyne.Window) f
 		fileSaver.SetFileName(fileMeta.FileName)
 		fileSaver.Show()
 	})
-	buttonSeparator := widget.NewSeparator()
-	backButton := widget.NewButtonWithIcon("", backArrow, func() {
-		w.SetContent(FilesPage(ctx, w, false))
+	copiedToClipboardText := widget.NewLabel("")
+	shareButton := widget.NewButtonWithIcon("", shareIcon, func() {
+		token := preferences.GetPreferences(ctx).String("token")
+		encryptionKey := preferences.GetPreferences(ctx).String("master_key")
+		masterKey, err := base64.StdEncoding.DecodeString(encryptionKey)
+		if err != nil {
+			panic(err)
+		}
+
+		viewInfo, err := bfsp.ShareFile(fileMeta, token, masterKey)
+		if err != nil {
+			panic(err)
+		}
+
+		viewInfoStr, err := bfsp.EncodeViewFileInfo(viewInfo)
+		if err != nil {
+			panic(err)
+		}
+
+		baseURL := config.BigCentralBaseURL()
+		url := fmt.Sprintf("%s/files/view_file/#z:%s", baseURL, viewInfoStr)
+
+		w.Clipboard().SetContent(url)
+
+		go func() {
+			copiedToClipboardText.SetText("Copied to clipboard")
+			time.Sleep(3 * time.Second)
+			copiedToClipboardText.SetText("")
+		}()
 	})
-	buttonBox := container.NewHBox(backButton, buttonSeparator, downloadButton)
+
+	buttonSeparator := widget.NewSeparator()
+	buttonBox := container.NewHBox(backButton, buttonSeparator, downloadButton, buttonSeparator, shareButton, copiedToClipboardText)
 
 	fileName := widget.NewLabel(fileMeta.FileName)
 	fileName.Wrapping = fyne.TextWrapWord
